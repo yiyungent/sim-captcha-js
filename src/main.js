cfc3982dcba214d5d111755b91d5128db946712e
yiyun <yiyungent@gmail.com>
2021-08-17 12:45:22 +0800

    chore: v0.1.1

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import "./css/sim-captcha.css";
 
 import SimCaptcha from "./js/sim-captcha.js";
 
-const VERSION = "0.1.0";
+const VERSION = "0.1.1";
 const GIT_HASH = "";
 console.log(
   `${"\n"} %c sim-captcha-js v${VERSION} ${GIT_HASH} %c https://github.com/yiyungent/sim-captcha-js ${"\n"}${"\n"}`,
